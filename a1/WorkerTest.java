2598c00876886c345977a78fa35d4fd3b6bf447d
Anupkh <anupkhadka67@hotmail.com>
2022-01-21 12:12:44 -0700

    base code

--- a1/WorkerTest.java
+++ a1/WorkerTest.java
@@ -2,10 +2,13 @@ package a1;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class WorkerTest {
     @Test
     public void testing(){
-
+        String s = "ti";
+        assertEquals("ti", s);
     }
 
 }
