53bcb5d5cda78defb61f6fd2e75f3dd997b63448
Anupkh <anupkhadka67@hotmail.com>
2022-01-22 10:12:56 -0700

    Qualification

--- a1/Qualification.java
+++ a1/Qualification.java
@@ -1,18 +1,46 @@
-//package a1;
-//
-//public class Qualification {
-//
-//	public Qualification(String s) {}
-//
-//	@Override
-//	public boolean equals(Object o) {}
-//
-//	@Override
-//	public String toString() {}
-//
-//	public Set<Worker> getWorkers() {}
-//
-//	public void addWorker(Worker w) {}
-//
-//	public void removeWorker(Worker w) {}
-//}
+package a1;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Qualification {
+	private String description;
+	private Set<Worker> workerSet= new HashSet<Worker>();
+
+	public Qualification(String s) {
+		this.description = s;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(this == o){
+			return true;
+		}
+		if(o == null){
+			return false;
+		}
+		Qualification qual = (Qualification)o;
+		if(this.description == qual.description){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return this.description;
+	}
+
+	public Set<Worker> getWorkers() {
+		return workerSet;
+	}
+
+	public void addWorker(Worker w) {
+		workerSet.add(w);
+	}
+
+	public void removeWorker(Worker w) {
+		workerSet.remove(w);
+	}
+}
