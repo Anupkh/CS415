4e2f3ac964b9b272f835bf09399eedcb14ff6f78
Anupkh <anupkhadka67@hotmail.com>
2022-01-22 17:56:23 -0700

    Qualification

--- a1/Worker.java
+++ a1/Worker.java
@@ -1,44 +1,96 @@
 package a1;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Worker {
     private String name;
     private Set<Qualification> qs;
     private double salary;
+    private Set<Project> projects = new HashSet<Project>();
 	public Worker(String name, Set<Qualification> qs, double salary){
         this.name = name;
         this.qs = qs;
         this.salary = salary;
     }
 
+	@Override
+	public boolean equals(Object o) {
+        if(this == o){
+            return true;
+        }
+        if(o == null){
+            return false;
+        }
+        Worker w1 = (Worker)o;
+        return this.name == w1.name;
+    }
+
 //	@Override
-//	public boolean equals(Object o) {}
+//	public String toString() {
+//        return this.name + ":" +
+//    }
 //
-//	@Override
-//	public String toString() {}
+	public String getName() {
+        return this.name;
+    }
 //
-//	public String getName() {}
-//
-//	public void setName(String s) {}
-//
-//	public double getSalary() {}
-//
-//	public void setSalary(double salary) {}
-//
-//	public Set<Qualification> getQualifications() {}
-//
-//	public void addQualification(Qualification q) {}
-//
-//	public Set<Project> getProjects() {}
-//
-//	public void addProject(Project p) {}
-//
-//	public void removeProject(Project p) {}
-//
-//	public int getWorkload() {}
-//
-//	public boolean willOverload(Project p) {}
-//
-//	public boolean isAvailable() {}
+	public void setName(String s) {
+        this.name = s;
+    }
+
+	public double getSalary() {
+        return this.salary;
+    }
+	public void setSalary(double salary) {
+        this.salary = salary;
+    }
+	public Set<Qualification> getQualifications() {
+        return this.qs;
+    }
+
+	public void addQualification(Qualification q) {
+        this.qs.add(q);
+    }
+
+	public Set<Project> getProjects() {
+        return projects;
+    }
+
+	public void addProject(Project p) {
+        this.projects.add(p);
+    }
+
+	public void removeProject(Project p) {
+        this.projects.remove(p);
+    }
+
+	public int getWorkload() {
+        int small = 0;
+        int medium = 0;
+        int large = 0;
+        for(Project p : projects){
+            if(p.getSize() == ProjectSize.SMALL){
+                small++;
+            }
+            if(p.getSize() == ProjectSize.MEDIUM){
+                medium++;
+            }
+            if(p.getSize() == ProjectSize.LARGE){
+                large++;
+            }
+        }
+        return ((3*large) + (2*medium) + small);
+    }
+
+	public boolean willOverload(Project p) {
+        projects.add(p);
+        int workLoad = getWorkload();
+        projects.remove(p);
+        return workLoad > 12;
+    }
+
+	public boolean isAvailable() {
+        return getWorkload() < 12;
+    }
 }
--- a1/WorkerTest.java
+++ a1/WorkerTest.java
@@ -2,13 +2,136 @@ package a1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class WorkerTest {
+    Worker w1 = new Worker("Bob", new HashSet<Qualification>(), 30000);
+    Worker w2 = new Worker("Sam", new HashSet<Qualification>(), 50000);
+
     @Test
-    public void testing(){
-        String s = "ti";
-        assertEquals("ti", s);
+    public void testEquals(){
+        assertTrue(w1.equals(w1));
+        assertFalse(w1.equals(w2));
+    }
+    @Test
+    public void testGetName(){
+        assertEquals("Bob", w1.getName());
     }
 
+    @Test
+    public void testSetName(){
+        Worker w3 = new Worker("Kate", new HashSet<Qualification>(), 100000);
+        w3.setName("Katie");
+        assertEquals("Katie", w3.getName());
+        w3.setName("");
+        assertEquals("", w3.getName());
+    }
+
+    @Test
+    public void testGetSalary(){
+        assertEquals(30000, w1.getSalary());
+    }
+
+    @Test
+    public void testSetSalary(){
+        w1.setSalary(20);
+        assertEquals(20, w1.getSalary());
+    }
+
+    @Test
+    public void testGetQualification(){
+        assertEquals(0, w1.getQualifications().size());
+    }
+
+    @Test
+    public void testAddQualification(){
+        assertEquals(0, w1.getQualifications().size());
+        Qualification q1 = new Qualification("Tools");
+        Qualification q2 = new Qualification("Computer");
+        w1.addQualification(q1);
+        assertEquals(1, w1.getQualifications().size());
+        w1.addQualification(q2);
+        assertEquals(2, w1.getQualifications().size());
+    }
+
+    @Test
+    public void testGetProjects(){
+        assertEquals(0, w1.getProjects().size());
+    }
+
+    @Test
+    public void testAddProjects(){
+        Project p1 = new Project("Build", w1.getQualifications(), ProjectSize.MEDIUM);
+        w1.addProject(p1);
+        assertEquals(1, w1.getProjects().size());
+    }
+
+    @Test
+    public void testRemoveProject(){
+        Project p1 = new Project("Build", w1.getQualifications(), ProjectSize.MEDIUM);
+        w1.addProject(p1);
+        assertEquals(1, w1.getProjects().size());
+        w1.removeProject(p1);
+        assertEquals(0, w1.getProjects().size());
+    }
+
+    @Test
+    public void testGetWorkload(){
+        Project p1 = new Project("Build", w1.getQualifications(), ProjectSize.LARGE);
+        Project p2 = new Project("Create", w1.getQualifications(), ProjectSize.MEDIUM);
+        Project p3 = new Project("Sleep", w1.getQualifications(), ProjectSize.SMALL);
+
+        assertEquals(0, w1.getWorkload());
+        w1.addProject(p1);
+        assertEquals(3, w1.getWorkload());
+        w1.addProject(p2);
+        assertEquals(5, w1.getWorkload());
+        w1.addProject(p3);
+        assertEquals(6, w1.getWorkload());
+        w1.addProject(p3);
+        assertEquals(6, w1.getWorkload());
+        w1.removeProject(p1);
+        w1.removeProject(p2);
+        assertEquals(1, w1.getWorkload());
+    }
+
+    @Test
+    public void testWillOverload(){
+        Project p1 = new Project("Build", w1.getQualifications(), ProjectSize.LARGE);
+        Project p2 = new Project("Destroy", w1.getQualifications(), ProjectSize.LARGE);
+        Project p3 = new Project("Create", w1.getQualifications(), ProjectSize.LARGE);
+        Project p4 = new Project("Sleep", w1.getQualifications(), ProjectSize.LARGE);
+        Project p5 = new Project("Eat", w1.getQualifications(), ProjectSize.LARGE);
+
+        w1.addProject(p1);
+        assertFalse(w1.willOverload(p2));
+        w1.addProject(p2);
+        assertFalse(w1.willOverload(p3));
+        w1.addProject(p3);
+        assertFalse(w1.willOverload(p4));
+        w1.addProject(p4);
+        assertTrue(w1.willOverload(p5));
+    }
+
+    @Test
+    public void testIsAvailable(){
+        Project p1 = new Project("Build", w1.getQualifications(), ProjectSize.LARGE);
+        Project p2 = new Project("Destroy", w1.getQualifications(), ProjectSize.LARGE);
+        Project p3 = new Project("Create", w1.getQualifications(), ProjectSize.LARGE);
+        Project p4 = new Project("Sleep", w1.getQualifications(), ProjectSize.LARGE);
+
+        assertTrue(w1.isAvailable());
+        w1.addProject(p1);
+        assertTrue(w1.isAvailable());
+        w1.addProject(p2);
+        assertTrue(w1.isAvailable());
+        w1.addProject(p3);
+        assertTrue(w1.isAvailable());
+        w1.addProject(p4);
+        assertFalse(w1.isAvailable());
+        w1.removeProject(p1);
+        assertTrue(w1.isAvailable());
+    }
 }
